061b4ea3277d61c771a4691512b3898642aeb556
Avirup Sett <72157037+Avirupsett@users.noreply.github.com>
2023-02-25 22:46:34 +0530

    fix : Route page

--- Frontend/moodify/src/Route/index.js
+++ Frontend/moodify/src/Route/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WebcamCapture from "../Container/Camera";
 import HomeContainer from '../Container/Home';
-// import Playlist from "../Container/Playlist";
+import Playlist from "../Container/Playlist";
 
 const RouterComponent = ()=>{
     return (
@@ -12,9 +12,9 @@ const RouterComponent = ()=>{
         <Routes>
 
           <Route path="/" element={<HomeContainer/>} />
-          {/* <Route path="/playlist" element={<Playlist/>} /> */}
+          <Route path="/playlist" element={<Playlist/>} />
           <Route path="/camera" element={<WebcamCapture/>} />
-        
+          {/* <Route path="/about" element={<AboutContainer/>} /> */}
 
 
         </Routes>
