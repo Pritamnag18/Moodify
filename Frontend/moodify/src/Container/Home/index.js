e87fba90586a8027a9ddaea5d99e3b324c6435d8
Pritamnag18 <pritamnag18@gmail.com>
2023-02-25 22:13:26 +0530

    fix: button

--- Frontend/moodify/src/Container/Home/index.js
+++ Frontend/moodify/src/Container/Home/index.js
@@ -16,10 +16,8 @@ const HomeContainer = () => {
                         <div className={`col-xl-6 col-lg-7 mt-2`} style={{ paddingLeft: '20px' }}>
                             <h1>Moodify</h1>
                             <h2>Get your Music Playlist <br /> according to your Mood</h2>
-                            <div className="button-one mt-4 pt-3">
-                                
-                                <button className="Primary btn btn-dark rounded-5 d-flex align-items-center"><Link to={'/camera'} style={{textDecoration:"none",color: "white"}}><IoCameraOutline size={28} style={{verticalAlign:'middle'}}/><span className="mx-2">OPEN YOUR CAMERA</span></Link></button>
-                                
+                            <div className="button-one mt-4 pt-3">  
+                                <button className="Primary btn btn-dark rounded-5 d-flex align-items-center"><Link to={'/camera'} style={{textDecoration:"none",color: "white"}}><IoCameraOutline size={28} style={{verticalAlign:'middle'}}/><span className="mx-2">OPEN YOUR CAMERA</span></Link></button>                               
                             </div>
                             <h3>CLICK  the above button to get your playlist.</h3>
                         </div>
